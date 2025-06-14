6460fff645340917e62f3f9919ce56eb36bbfaef
sahilempire <gsahil.sg20@gmail.com>
2025-06-14 11:39:46 +0530

    api fix

--- landing-page/app/layout.tsx
+++ landing-page/app/layout.tsx
@@ -83,6 +83,10 @@ export default function RootLayout({
             </head>
             <body className={`bg-[#F8F7F3] grain-texture ${fustat.variable} ${spaceMono.variable}`}>
                 <Providers>{children}</Providers>
+                <Script
+                    src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+                    strategy="beforeInteractive"
+                />
             </body>
         </html>
     );
